5111ee35c8b02e231ca8be41d8b58b38641b30a9
kymin <park48664866@gmail.com>
2026-04-15 18:51:43 +0900

    feat: 엔드포인트 및 요청 메서드 어노테이션 추가

--- src/main/java/org/sopt/post/controller/PostController.java
+++ src/main/java/org/sopt/post/controller/PostController.java
@@ -4,19 +4,34 @@ import org.sopt.global.exception.BaseException;
 import org.sopt.global.response.CommonResponse;
 import org.sopt.global.status.SuccessStatus;
 import org.sopt.post.controller.mapper.PostPresentationMapper;
-import org.sopt.post.dto.response.PostDetailResponse;
-import org.sopt.post.dto.response.PostListResponse;
+import org.sopt.post.service.dto.response.PostDetailResponse;
+import org.sopt.post.service.dto.response.PostListResponse;
 import org.sopt.post.model.input.CreatePostInput;
 import org.sopt.post.model.input.UpdatePostInput;
 import org.sopt.post.model.output.PostDetailOutput;
 import org.sopt.post.model.output.PostListOutput;
 import org.sopt.post.service.PostService;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping(path = "/api/v1/posts")
 public class PostController {
-	private final PostService postService = new PostService();
+	private final PostService postService;
+
+	public PostController(PostService postService) {
+		this.postService = postService;
+	}
 
 	// POST /posts
-	public CommonResponse<PostDetailOutput> createPost(CreatePostInput input) {
+	@PostMapping
+	public CommonResponse<PostDetailOutput> createPost(@RequestBody CreatePostInput input) {
 		try {
 			input.validate();
 			PostDetailResponse response = postService.createPost(PostPresentationMapper.toRequest(input));
@@ -27,13 +42,15 @@ public class PostController {
 	}
 
 	// GET /posts
+	@GetMapping
 	public CommonResponse<PostListOutput> getAllPosts() {
 		PostListResponse response = postService.getAllPosts();
 		return CommonResponse.success(SuccessStatus.POST_LIST_FOUND, PostPresentationMapper.toListOutput(response));
 	}
 
 	// GET /posts/{id}
-	public CommonResponse<PostDetailOutput> getPost(long id) {
+	@GetMapping(path = "/{postId}")
+	public CommonResponse<PostDetailOutput> getPost(@PathVariable(name = "postId") long id) {
 		try {
 			PostDetailResponse response = postService.getPost(id);
 			return CommonResponse.success(SuccessStatus.POST_FOUND, PostPresentationMapper.toDetailOutput(response));
@@ -43,7 +60,11 @@ public class PostController {
 	}
 
 	// PUT /posts/{id}
-	public CommonResponse<PostDetailOutput> updatePost(UpdatePostInput input) {
+	@PutMapping(path = "/{postId}")
+	public CommonResponse<PostDetailOutput> updatePost(
+			@PathVariable(name = "postId") long id,
+			@RequestBody UpdatePostInput input
+	) {
 		try {
 			input.validate();
 			PostDetailResponse response = postService.updatePost(input.id(), PostPresentationMapper.toRequest(input));
@@ -54,7 +75,8 @@ public class PostController {
 	}
 
 	// DELETE /posts/{id}
-	public CommonResponse<Void> deletePost(long id) {
+	@DeleteMapping(path = "/{postId}")
+	public CommonResponse<Void> deletePost(@PathVariable(name = "postId") long id) {
 		try {
 			postService.deletePost(id);
 			return CommonResponse.success(SuccessStatus.POST_DELETED);
