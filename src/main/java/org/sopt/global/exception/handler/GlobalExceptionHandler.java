95a0d9852aaede9a765cbafb4a90ea4da799aa8f
kymin <park48664866@gmail.com>
2026-04-22 10:55:56 +0900

    feat: 파라미터 예외 처리 추가

--- src/main/java/org/sopt/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/org/sopt/global/exception/handler/GlobalExceptionHandler.java
@@ -5,8 +5,10 @@ import org.sopt.global.response.ApiResponse;
 import org.sopt.global.status.code.FailureCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends BaseExceptionHandler{
@@ -22,6 +24,16 @@ public class GlobalExceptionHandler extends BaseExceptionHandler{
 			return buildErrorResponse(be.getFailureStatus());
 		}
 		return buildErrorResponse(FailureCode.INTERNAL_ERROR);
+	}
+
+	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
+	protected ResponseEntity<ApiResponse<Void>> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+		return buildErrorResponse(FailureCode.INVALID_REQUEST_PARAMETER);
+	}
+
+	@ExceptionHandler(MissingServletRequestParameterException.class)
+	protected ResponseEntity<ApiResponse<Void>> handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
+		return buildErrorResponse(FailureCode.INVALID_REQUEST_PARAMETER);
 	}
 
 	@ExceptionHandler(Exception.class)
